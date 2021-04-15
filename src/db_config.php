22610f34d8331065832934fefb9d7f7920a70147
Ieva Markunaite <markunaite.ieva@gmail.com>
2021-04-15 11:34:20 +0300

    sukurtas atskiras failas DB prisijungimams

---
+++ src/db_config.php
@@ -0,0 +1,17 @@
+<?php
+ // docker DB
+ $host = "localhost_db";  
+ $user = "root";  
+ $pass = "nera";  
+ $dbName = "task_system";  
+ $message = "";  
+
+ 
+// Xampp DB
+ //  $host = "localhost";  
+//  $user = "root";  
+//  $pass = "";  
+//  $dbName = "task_system";  
+//  $message = ""; 
+
+ ?>
