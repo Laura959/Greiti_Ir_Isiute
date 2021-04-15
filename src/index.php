1fcee405930952264facb71d02abbc1e73e4fd23
Ieva Markunaite <markunaite.ieva@gmail.com>
2021-04-15 11:36:12 +0300

    pridėti minimalūs stiliai

--- src/index.php
+++ src/index.php
@@ -1,91 +1,66 @@
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Aldrich&display=swap" rel="stylesheet">
-        <title>PARDĖ</title>
-    </head>
-    <body>
-			<div class="header">
-		<h2>Login</h2>
-	</div>
-        <form action="index.php"method="POST">
-					<div class="input-group">
-						<label>Vartotojo Vardas</label>
-						<input type="text" name="Username" placeholder="Iveskite gmail" >
-					</div>
-					<div class="input-group">
-						<label>Splatazodis</label>
-						<input type="password" name="passwordas" placeholder="Iveskite Slaptazodi">
-					</div>
-            <input type="submit" name="login" value="login"><br>
-
-            <?php
+<?php
 session_start();
+include_once('db_config.php');
 
-$dbhost= "localhost";
-$dbuser= "root";
-$dbpass= "";
-$dbname= "shop";//Savo db varda ideti gali
-
-
-
-if(!$conn= mysqli_connect($dbhost,$dbuser,$dbpass,$dbname)){
-
-
-{
-
-        die("failed to connect!");
-    }
-
+try {
+     $connect = new PDO("mysql:host=$host; dbname=$dbName", $user, $pass);
+     $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     if (isset($_POST["login"])) {
+          //  if(empty($_POST["username"]) || empty($_POST["password"]))  
+          //  {  
+          //       $message = '<span>All fields are required</span>';  
+          //  }  
+          //  else  
+          //  {  
+          $query = "SELECT * FROM vartotojai WHERE El_pastas = ? AND Slaptazodis = ?";
+          $statement = $connect->prepare($query);
+          $statement->execute([$_POST["username"], $_POST["password"]]);
+          $count = $statement->rowCount();
+          if ($count > 0) {
+               $statement->setFetchMode(2);
+               $result = $statement->fetchAll();
+               $_SESSION["name"] = $result[0]['Vardas'];
+               header("location:main.php");
+          } else {
+               $message = 'Invalid username or password';
+          }
+          //  }  
+     }
+} catch (PDOException $error) {
+     print_r($error);
+     $message = 'Something went wrong';
 }
-
-
-    if($_SERVER['REQUEST_METHOD']== "POST")
-    {
-
-      $username=$_POST['Username'];
-      $pwd=$_POST['passwordas'];
-
-      if(!empty($username) && !empty($pwd))
-      {
-          //Skaito is db/
-
-          $query ="select * from vartotojai1 where Vardas='$username' limit 1";
-          $result= mysqli_query($conn, $query);
-
-          if($result)
-          {
-               if($result && mysqli_num_rows($result)> 0)
-     {
-              $user_data=mysqli_fetch_assoc($result);
-
-                  if($user_data['Slaptazodis']===$pwd)
-              {
-                      $_SESSION['Username']= $user_data['Username'];
-                      header("Location: index1.php");
-                          die;
-
-          }
-          else {
-            echo "Neteisingas Slaptazodis";
-          }
-          }
-          else {
-            echo "Neteisingas Vartotojo Vardas";
-          }
-          }
-
-          }else
-          {
-              echo "<br>Uzpildykite visus langelius";
-          }
-  }
-
-
 ?>
-        </form>
-    </div>
-    </body>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <meta name="description" content="Login page">
+     <title>Login</title>
+     <link href="css/style.css?rnd=132" type="text/css" rel="stylesheet">
+     <link rel="preconnect" href="https://fonts.gstatic.com">
+     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;500&display=swap" rel="stylesheet">
+</head>
+
+<body>
+     <div class="header">
+          <h2>Login</h2>
+     </div>
+     <form class="form" method="POST" id="login-form">
+          <label>Email</label>
+          <input type="text" name="username" placeholder="Your email" pattern="^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$" title="Enter a valid email address" required>
+          <label>Password</label>
+          <input type="password" name="password" placeholder="Your password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required>
+          <input type="submit" name="login" value="Login" class="login-btn" id="login-btn"><br>
+          <?php
+          if (!empty($message)) {
+               echo '<p class="error">' . $message . '</p><br>';
+          }
+          ?>
+     </form>
+     </div>
+</body>
+
 </html>
