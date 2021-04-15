21946b136d426878340f1d8b40a040261306203a
Ieva Markunaite <markunaite.ieva@gmail.com>
2021-04-15 11:43:00 +0300

    DB prisijungimas

--- src/db_config.php
+++ src/db_config.php
@@ -1,17 +1,17 @@
 <?php
- // docker DB
- $host = "localhost_db";  
- $user = "root";  
- $pass = "nera";  
- $dbName = "task_system";  
- $message = "";  
+//  // docker DB
+//  $host = "localhost_db";  
+//  $user = "root";  
+//  $pass = "nera";  
+//  $dbName = "task_system";  
+//  $message = "";  
 
  
 // Xampp DB
- //  $host = "localhost";  
-//  $user = "root";  
-//  $pass = "";  
-//  $dbName = "task_system";  
-//  $message = ""; 
+$host = "localhost";  
+ $user = "root";  
+ $pass = "";  
+ $dbName = "task_system";  
+ $message = ""; 
 
  ?>
