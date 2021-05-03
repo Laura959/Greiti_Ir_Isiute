322b715e0b6f18f494c9b219a4e0295193c3ce29
Ieva Markunaite <markunaite.ieva@gmail.com>
2021-05-03 08:55:05 +0300

    pakeitimas pagal DOCKER

--- src/classes/dbh.class.php
+++ src/classes/dbh.class.php
@@ -17,3 +17,26 @@ class Dbh {
         }
     }
 }
+
+// Docker
+
+// class Dbh {
+//     private $host = "localhost_db";  
+//     private $user = "root";
+//     private $pass = "nera";
+//     private $dbName = "projektas";
+
+//     public function connect(){
+//         try{
+//             $dsn = "mysql:host=".$this->host.";dbname=".$this->dbName;
+//             $pdo = new PDO($dsn, $this->user, $this->pass);
+//             // $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+//             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//             return $pdo;
+//         }catch(PDOException $error){
+//             return $error;
+//         }
+//     }
+// }
+
+?>
