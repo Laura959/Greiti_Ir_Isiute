7f63f1dc0e42edec564147445275198cef57c02d
Vytautas <70965403+vytkuklys@users.noreply.github.com>
2021-05-04 16:44:08 +0300

    modify db variables

--- src/classes/dbh.class.php
+++ src/classes/dbh.class.php
@@ -1,5 +1,7 @@
 <?php
 class Dbh {
+    // private $host = "localhost_db";  
+    // private $pass = "nera";
     private $host = "localhost";  
     private $user = "root";
     private $pass = "";
@@ -17,26 +19,3 @@ class Dbh {
         }
     }
 }
-
-// Docker
-
-// class Dbh {
-//     private $host = "localhost_db";  
-//     private $user = "root";
-//     private $pass = "nera";
-//     private $dbName = "projektas";
-
-//     public function connect(){
-//         try{
-//             $dsn = "mysql:host=".$this->host.";dbname=".$this->dbName;
-//             $pdo = new PDO($dsn, $this->user, $this->pass);
-//             // $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-//             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//             return $pdo;
-//         }catch(PDOException $error){
-//             return $error;
-//         }
-//     }
-// }
-
-?>
